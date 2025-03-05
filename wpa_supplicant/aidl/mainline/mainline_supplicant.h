74d3938066e8d0e10bc00afb80af12fbff01a851
Gabriel Biren <gbiren@google.com>
2025-03-05 10:25:00 -0800

    Revert "Revert "Return an interface object from addUsdInterface ..."
    Revert submission 32132699-revert-32029057-mainline-supplicant-generalize-iface-type-NJLGVJHNTR
    
    Reason for revert: Located the root cause of the build failure
    
    Reverted changes: /q/submissionid:32132699-revert-32029057-mainline-supplicant-generalize-iface-type-NJLGVJHNTR
    
    Change-Id: I637129af973da4e6600153c2dfccd463b3380098

--- wpa_supplicant/aidl/mainline/mainline_supplicant.h
+++ wpa_supplicant/aidl/mainline/mainline_supplicant.h
@@ -9,9 +9,12 @@
 #ifndef MAINLINE_SUPPLICANT_IMPL_H
 #define MAINLINE_SUPPLICANT_IMPL_H
 
-#include <set>
+#include <map>
+
+#include "usd_iface.h"
 
 #include <aidl/android/system/wifi/mainline_supplicant/BnMainlineSupplicant.h>
+#include <aidl/android/system/wifi/mainline_supplicant/IUsdInterface.h>
 #include <aidl/android/system/wifi/mainline_supplicant/SupplicantStatusCode.h>
 
 extern "C"
@@ -24,20 +27,22 @@ extern "C"
 }
 
 using ::aidl::android::system::wifi::mainline_supplicant::BnMainlineSupplicant;
+using ::aidl::android::system::wifi::mainline_supplicant::IUsdInterface;
 using ::aidl::android::system::wifi::mainline_supplicant::SupplicantStatusCode;
 
 class MainlineSupplicant : public BnMainlineSupplicant {
     public:
         MainlineSupplicant(struct wpa_global* global);
-        ndk::ScopedAStatus addUsdInterface(const std::string& ifaceName);
+        ndk::ScopedAStatus addUsdInterface(const std::string& ifaceName,
+            std::shared_ptr<IUsdInterface>* _aidl_return);
         ndk::ScopedAStatus removeUsdInterface(const std::string& ifaceName);
         ndk::ScopedAStatus terminate();
 
     private:
         // Raw pointer to the global structure maintained by the core
         struct wpa_global* wpa_global_;
-        // Names of all active USD interfaces
-        std::set<std::string> active_usd_ifaces_;
+        // Map containing all active USD interfaces, mapped by iface name -> object
+        std::map<std::string, std::shared_ptr<IUsdInterface>> active_usd_ifaces_;
 };
 
 #endif  // MAINLINE_SUPPLICANT_IMPL_H
